0c04ec703e75899d18f7285bd5feca7671ff520c
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2023-09-04 06:45:06 +0700

    part 1-2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -14,6 +14,26 @@ end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.count == 0  # if no array
+    return 0
+  end
+
+  max1 = 0
+  max2 = 0
+  if arr.min < 0  # if negative number
+    max1 = arr.min
+    max2 = arr.min
+  end
+
+  for i in arr
+    if i > max1
+      max2 = max1
+      max1 = i
+    elsif i > max2
+        max2 = i
+    end
+  end
+  return max1 + max2
 end
 
 def sum_to_n? arr, n
